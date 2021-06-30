dbc793693547e71f613dea5779ac3131a3a6cd8e
Jonathan Holloway <loadtheaccumulator@gmail.com>
2021-06-30 00:02:19 -0500

    add find_packages
    Signed-off-by: Jonathan Holloway <loadtheaccumulator@gmail.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(use_scm_version=True)
+setup(use_scm_version=True,
+      packages=find_packages(include=['kafkavents_reportportal'])
+      )
